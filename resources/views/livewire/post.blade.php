c3646d43751850820369fc6f98cd80b46c4d48a0
GeoSot <geo.sotis@gmail.com>
2024-02-01 16:38:12 +0200

    add styles

--- resources/views/livewire/post.blade.php
+++ resources/views/livewire/post.blade.php
@@ -2,7 +2,7 @@
     <div class="w-full">
         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900 dark:text-gray-100">
-                <livewire:create-post componentTitle="This is a component name"/>
+                <livewire:create-post componentTitle="This is a component name" wire:id="foo"/>
             </div>
         </div>
     </div>
